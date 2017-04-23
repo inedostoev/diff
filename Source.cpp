b078168434518f625e9e8b14b9a1d19513002d7e
inedostoev <inedostoev@gmail.com>
2017-04-23 18:02:38 +0300

    main

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define NDEBUG
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "diff.h"
+
+int main()
+{   
+	//FILE* stream = fopen("configFile.txt", "r");
+	diff d;
+	d.execute();
+	return 0;
+}
