df77263c01d9bf763761523e87dee33e58026aeb
inedostoev <inedostoev@gmail.com>
2017-04-27 22:45:50 +0300

    header diff

---
+++ include/diff.h
@@ -0,0 +1,59 @@
+#ifndef TREE_H_INCLUDED
+#define TREE_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <unistd.h>
+#include "./Node/Node.h"
+
+const int MAX_OPER_OR_FUNC_SIZE = 16;
+enum Traverse {
+    INORDER,
+    PREORDER,
+    POSTORDER
+};
+
+enum Priority {
+    LOW_PRIOR,
+    ADD_AND_SUB_PRIOR,
+    MUL_AND_DIV_PROIR,
+    EXP_PRIOR,
+};
+
+class diff
+{
+public:
+                    diff();
+                    ~diff();
+    FILE            *inputFile_;
+    FILE            *outputFile_;
+    
+		    diff& operator = (const diff&);
+    		    diff(const diff&);
+    void            execute();
+                
+private:
+    Node*           Tree_; 
+
+    Node*           buildTree(char* buffer, size_t *counter, Node* Darth_Vader);
+    Node*           copyTree(Node* node, Node* parent);
+    Node*           differentiation(Node* node, Node* Parent);
+    Node*           transformDiffTree(Node* diffNode, Node* parent);
+
+    dataType        getDataType(char* data);
+    size_t          getFileSize();
+   
+    void            copyStr(Node* node, char* copyStr);
+    char*           makeOperPtr(char Oper);
+    void            dumpTree(FILE* stream, Node* node);
+    void            dotDump(Node* root);
+    void            dotNodeDump(Node* root, FILE* stream);
+    void            visitor(Traverse mode, Node* node, void act(Node*));
+    void            makeTex(Node* root, Node* first);
+    void            makeTexNodeDump(Node* node, FILE* stream);
+    Priority        getPriority(Node* node);
+};
+
+#endif // !TREE_H_INCLUDED
