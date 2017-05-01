b71ad2faba85793622244b9629ce01c9287d7459
inedostoev <inedostoev@gmail.com>
2017-05-01 16:33:16 +0300

    [ver 3.0] TransformTree, add define

--- include/transformDiffTree.cpp
+++ include/transformDiffTree.cpp
@@ -1,51 +1,96 @@
 #include "diff.h"
+#include <math.h>
+const double accuracy = 0.001;
 
 Node* diff::transformDiffTree(Node* diffNode, Node* parent) {
     if(diffNode == NULL) return NULL;
-    Node* newDiffNode = NULL;
-//---------------------------------------------------------//    
+    Node* newDiffNode = NULL;    
+//*********************************************************//
+#define RIGHTCOPY copyTree(diffNode->right_, parent);
+#define LEFTCOPY copyTree(diffNode->left_, parent);
+#define LEFTZEROEQUAL                                   \
+        fabs(diffNode->left_->dataNum_) < accuracy 
+#define RIGHTZEROEQUAL                                  \
+        fabs(diffNode->right_->dataNum_) < accuracy
     if(diffNode->dataStr_ != NULL) {
-        if(!strcmp(diffNode->dataStr_, "*") 
-                && (diffNode->right_->dataNum_ == 0 || diffNode->left_->dataNum_ == 0)) {
-            Node* newTrfNode = new Node(parent, 0.0, NUMBER); 
-            newTrfNode->left_ = NULL;
-            newTrfNode->right_= NULL;
+#define MUL !strcmp(diffNode->dataStr_, "*")
+//---------------------------------------------------------//
+    if(MUL && (LEFTZEROEQUAL || RIGHTZEROEQUAL)) {
+        Node* newTrfNode = new Node(parent, 0.0, NUMBER); 
+        newTrfNode->left_ = NULL;
+        newTrfNode->right_= NULL;
+        return newTrfNode;
+    }
+//--------------------------------------------------------//
+#define RIGHTONEEQUAL                                   \
+        fabs(diffNode->right_->dataNum_ - 1) < accuracy 
+        if(MUL && RIGHTONEEQUAL) {
+           Node* newTrfNode = LEFTCOPY;       
             return newTrfNode;
         }
-
-        if(!strcmp(diffNode->dataStr_, "*") && diffNode->right_->dataNum_ == 1) {
-           Node* newTrfNode = copyTree(diffNode->left_, parent);       
-            return newTrfNode;
-        }
+#undef RIGHTONEEQUAL
+//--------------------------------------------------------//
+#define LEFTONEEQUAL                                    \
+        fabs(diffNode->left_->dataNum_ - 1) < accuracy
         else
-        if(!strcmp(diffNode->dataStr_, "*") && diffNode->left_->dataNum_ == 1) {
-            Node* newTrfNode = copyTree(diffNode->right_, parent);
+        if(MUL && LEFTONEEQUAL) {
+            Node* newTrfNode = RIGHTCOPY;
             return newTrfNode;
         }
+#undef LEFTONEEQUAL
+#undef MUL        
+//--------------------------------------------------------//
+#define ADD !strcmp(diffNode->dataStr_, "+")  
         else
-        if(!strcmp(diffNode->dataStr_, "+") && diffNode->left_->dataNum_ == 0) {
-            Node* newTrfNode = copyTree(diffNode->right_, parent);
+        if(ADD && LEFTZEROEQUAL) {
+            Node* newTrfNode = RIGHTCOPY;
             return newTrfNode;
         }   
+//--------------------------------------------------------//
         else
-        if(!strcmp(diffNode->dataStr_, "+") && diffNode->right_->dataNum_ == 0) {
-            Node* newTrfNode = copyTree(diffNode->left_, parent);
+        if(ADD && RIGHTZEROEQUAL) {
+            Node* newTrfNode = LEFTCOPY;
             return newTrfNode;
         } 
+#undef ADD
+//-------------------------------------------------------//
+#define SUB !strcmp(diffNode->dataStr_, "-")    
         else
-        if(!strcmp(diffNode->dataStr_, "-") && diffNode->right_->dataNum_ == 0) {
-            Node* newTrfNode = copyTree(diffNode->left_, parent);
+        if(SUB && RIGHTZEROEQUAL) {
+            Node* newTrfNode = LEFTCOPY;
             return newTrfNode;
         }
 //-------------------------------------------------------//
+        else
+        if(SUB && LEFTZEROEQUAL) {
+        Node* newTrfNode = 
+            new Node(parent, makeOperPtr('*'), OPERAND); 
+        newTrfNode->left_ = 
+            new Node(newTrfNode, -1, NUMBER);
+        newTrfNode->right_ = 
+            copyTree(diffNode->right_, newTrfNode);
+        return newTrfNode;       
+        }
+#undef SUB
+//-------------------------------------------------------//
     }
+#undef LEFTZEROEQUAL
+#undef RIGHTZEROEQUAL
+#undef RIGHTCOPY
+#undef LEFTCOPY
+//*******************************************************//
+#define NEWNODE(arg)                        \
+    new Node(parent, arg, diffNode->type_)
+#define TRANSFORM(son)                              \
+    transformDiffTree(son, newDiffNode)
+
     if(diffNode->dataStr_ == NULL)
-       newDiffNode  = new Node(parent, diffNode->dataNum_, diffNode->type_);
-    else { 
-        newDiffNode = new Node(parent, copyStr(diffNode->dataStr_), diffNode->type_);
-    }
-    newDiffNode->left_ = transformDiffTree(diffNode->left_, newDiffNode);
-    newDiffNode->right_ = transformDiffTree(diffNode->right_, newDiffNode);
+        newDiffNode  = NEWNODE(diffNode->dataNum_);
+    else 
+        newDiffNode = NEWNODE(copyStr(diffNode->dataStr_));
+    newDiffNode->left_ = TRANSFORM(diffNode->left_);
+    newDiffNode->right_ = TRANSFORM(diffNode->right_);
     return newDiffNode;  
 }
-
+#undef TRANSFORM
+#undef NEWNODE
